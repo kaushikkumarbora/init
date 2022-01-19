0ed8cbc2f4851d5fe2583dd3d08adfd1d1058bbf
Kaushik Kumar Bora <kaushikkumarbora@gmail.com>
2022-01-19 12:52:19 +0530

    Added Python init

---
+++ python.sh
@@ -0,0 +1,20 @@
+# PIP Config Setup
+
+# Create virtual environment for out prooject
+python -m venv venv
+
+# uses the license package to download a license of choice, in
+# this case the MIT license
+
+#TODO
+
+# uses the gitignore package to automatically download the relevant
+# .gitignore file from GitHub’s repo
+
+curl https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore -OutFile .gitignore
+
+# uses the covgen package to generate the Contributor Covenant and
+# give your project a code of conduct that will be welcoming to all
+# contributors
+
+#TODO
